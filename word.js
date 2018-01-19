97d55cb22a649c5b1d4797c098cb4d297c362320
Pace <pacemarier@spacesandwich.me>
2018-01-19 17:23:35 -0500

    tidied up the code

--- word.js
+++ word.js
@@ -1,5 +1,6 @@
-//
+// word of the minute experiment
 
+// countdown timer in the menu bar
 var myVar = setInterval(function(){ myTimer() }, 1000);
 
 function myTimer() {
@@ -7,35 +8,38 @@ function myTimer() {
   var s = d.getSeconds();
   var deadline = 59;
   var t = deadline - s;
-  document.getElementById("demo").innerHTML = 'New word in ' + t + ' seconds';
+  document.getElementById("countdown").innerHTML = 'New word in ' + t + ' seconds';
 
 }
 
-
+//get a random word through the wordnik api
 function randomWord(callback) {
     var baseUrl = "https://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=true&includePartOfSpeech=noun&minCorpusCount=1000&minDictionaryCount=4&api_key=";
     var apiKey = "1380d58b8b5c33325130c0e8f340be6bc6fba6f7bb65bfc6f";
     var apiUrl = baseUrl + apiKey;
 
-    //A promise is needed here, as without it the second API call would return before pronounceIt() can execute the callback, and we would get, well, nothing, instead of the actual word.
+    //wo a Promise the 2nd wordnik api call will return before pronounceIt() executes the callback, which would give us nothing.
     return new Promise(function(resolve, reject) {
       $.ajax({
           type: "GET",
           url: apiUrl,
           dataType: "json",
           success: function(data) {
-              $("#mot1").append(data.word);
+              //write the data to the <h1> tag
+              $("#randomWord").append(data.word);
               resolve();
           }
           });
       });
 
 }
+
+//pull the words pronounciation from the wordnik api
 function pronounceIt() {
     var baseUrl = "https://api.wordnik.com/v4/word.json/"
     var apiKey = "1380d58b8b5c33325130c0e8f340be6bc6fba6f7bb65bfc6f";
     //var word = $("#mot1").text();
-    var word = $("#mot1").text();
+    var word = $("#randomWord").text();
     var apiUrl = baseUrl + word + "/pronunciations?useCanonical=true&typeFormat=ahd&limit=1&api_key=" + apiKey;
 
     $.ajax({
@@ -46,16 +50,18 @@ function pronounceIt() {
             if (data.length > 0) {
               var input = data[0].raw;
               var output = "[<em>" + input.slice(1,-1) + "</em>]";
-              $("#quoi1").append(output);
+              //write data to <li> tag
+              $("#pronounceWord").append(output);
               console.log(input);
             }
         }
     });
 }
+// get the word type and pronounciation
 function defineIt() {
     var baseUrl = "https://api.wordnik.com/v4/word.json/"
     var apiKey = "1380d58b8b5c33325130c0e8f340be6bc6fba6f7bb65bfc6f";
-    var word = $("#mot1").text();
+    var word = $("#randomWord").text();
     //var word = "test";
     var apiUrl = baseUrl + word + "/definitions?limit=1&includeRelated=true&sourceDictionaries=all&useCanonical=true&includeTags=false&api_key=" + apiKey;
 
@@ -64,34 +70,45 @@ function defineIt() {
         url: apiUrl,
         dataType: "json",
         success: function(data) {
-          $("#hein").append(data[0].text);
-          $("#leur").append("<strong>" + data[0].partOfSpeech + "</strong>");
+          //writes definition to a <div> tag
+          $("#defineWord").append(data[0].text);
+          //writes word type (non, verb, ect) to an <li> tag
+          $("#wordType").append("<strong>" + data[0].partOfSpeech + "</strong>");
           resolve();
         }
     });
 }
 
-
+//runs the functions when the document opens
 $(document).ready(function() {
+  // initial run. always runs the word grabbing function in this order
   randomWord().then(pronounceIt).then(defineIt);
+  // fades the word and definition onto the page
   $("#word").fadeIn(3000);
+
+  //sets a timer
   var meVar = setInterval(function(){ meTime() }, 1000);
 
   function meTime() {
     var dat = new Date();
-    var sec = dat.getSeconds();
-    var deadLine = 59;
-    var temp = deadLine - sec;
+    var sec = dat.getSeconds(); //retrieves only the seconds portion
+    var deadLine = 59; // 59 sec in a minute (starts at 0)
+    var temp = deadLine - sec; //remainder is the amount of seconds in a minute minus the current seconds portion of the time
+    //if there is less than 6 seconds left in the minute
     if(temp < 6){
-      $("#word").fadeOut(5000);
+      $("#word").fadeOut(5000); //start fading out the word
     }
+    //if there is 0 seconds left
     if(temp <= 0){
-      document.getElementById("mot1").innerHTML = '';
-      document.getElementById("quoi1").innerHTML = '';
-      document.getElementById("hein").innerHTML = "";
-      document.getElementById("leur").innerHTML = "";
-    randomWord().then(pronounceIt).then(defineIt);
-    $("#word").fadeIn(2000);
+      //clears all of the word information
+      document.getElementById("randomWord").innerHTML = '';
+      document.getElementById("wordType").innerHTML = '';
+      document.getElementById("pronounceWord").innerHTML = "";
+      document.getElementById("defineWord").innerHTML = "";
+      // runs the random word functions
+      randomWord().then(pronounceIt).then(defineIt);
+      // fades in the word
+      $("#word").fadeIn(2000);
   }
 
   }
