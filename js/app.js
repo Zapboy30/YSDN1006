9e293629421aff890ce87809f65da2c3c77f4585
Pace <pacemarier@spacesandwich.me>
2018-02-09 22:18:03 -0500

    Fixed Responsive Intro Placement

--- js/app.js
+++ js/app.js
@@ -12,7 +12,11 @@ $("a").on('click', function(event) { // when an anchor is clicked.
     }
 });
 
+
+
 $(document).on("scroll", function(){
+      var widthNav = $(window).width();
+
   if
     ($(document).scrollTop() > 25){
     $("nav").addClass("nav-shrink");
@@ -23,21 +27,41 @@ $(document).on("scroll", function(){
     $( ".container" ).css("-webkit-transition", "padding 0.5s ease-in-out");
     $( ".container" ).css("-moz-transition", "padding 0.5s ease-in-out");
     $( ".container" ).css( "padding-top", "156px" );
+    $( ".lg-nav-intro" ).css( "margin", "0" );
   }
   else
   {
     $("nav").removeClass("nav-shrink");
-    $("nav").css("transition", "all 0.5s ease-in-out");
-    $("nav").css("-webkit-transition", "all 0.5s ease-out");
-    $("nav").css("-moz-transition", "all 0.5s ease-out");
-    $( ".container" ).css("transition", "padding 0.55s ease-in-out");
-    $( ".container" ).css("-webkit-transition", "padding 0.55s ease-in-out");
-    $( ".container" ).css("-moz-transition", "padding 0.55s ease-in-out");
+    $("nav").css("transition", "all 0.55s ease-in-out");
+    $("nav").css("-webkit-transition", "all 0.55s ease-out");
+    $("nav").css("-moz-transition", "all 0.55s ease-out");
+    $( ".container" ).css("transition", "padding 0.5s ease-in-out");
+    $( ".container" ).css("-webkit-transition", "padding 0.5s ease-in-out");
+    $( ".container" ).css("-moz-transition", "padding 0.5s ease-in-out");
     $( ".container" ).css( "padding-top", "400px" );
+
+
+
+	       if ( widthNav < 480 ){
+           $( ".lg-nav-intro" ).css( "margin", "175px 0 175px 0" );
+           $( ".lg-nav-intro" ).css( "height", "50px" );
+	    } if (widthNav < 768 && widthNav > 480){
+        $( ".lg-nav-intro" ).css( "margin", "166px 0 166px 0" );
+        $( ".lg-nav-intro" ).css( "height", "68px" );
+      } if (widthNav < 960 && widthNav > 768){
+        $( ".lg-nav-intro" ).css( "margin", "146px 0 146px 0" );
+        $( ".lg-nav-intro" ).css( "height", "108px" );
+      }	else if (widthNav > 960){
+        $( ".lg-nav-intro" ).css( "margin", "131px 0 131px 2%" );
+        $( ".lg-nav-intro" ).css( "height", "138px" );
+ }
+
+
   }
 });
 
 $(document).ready(function(){
+
       // super sexy nav fade in and out
   $('#nav-close').click(function() {
       $("#nav-overlay").fadeOut(500);
