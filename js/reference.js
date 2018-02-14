59930388bcc16f64d65800e5354e48ed77f654b0
Valery <valerymarier@cosmicmocha.com>
2018-02-14 14:17:24 -0500

    Added web references

---
+++ js/reference.js
@@ -0,0 +1,8 @@
+//bouncy subheader text
+$(function(){
+
+  var subtitle = ["As seen on TV!", "99.9% Vegan!", "Now In Stunning 240p!", "Lorem Ipsum", "Now 80% Off!"];
+
+  $("#subheader").html($("#subheader").html().replace("placeholder", subtitle[Math.floor(Math.random()*subtitle.length)]));
+
+});
