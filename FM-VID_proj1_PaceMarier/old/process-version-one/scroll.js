0f9a86ea9be928c922413d8eb4c1764c8a22eaf9
Valery <pacemarier99@hotmail.com>
2018-02-19 11:55:36 -0500

    Bugginess on the Desktop App Sync

---
+++ FM-VID_proj1_PaceMarier/old/process-version-one/scroll.js
@@ -0,0 +1,13 @@
+// scroll.js
+// for scrolling that is smoother than jazz.
+
+$("a").on('click', function(event) { // when an anchor is clicked.
+  event.preventDefault();
+    if (this.hash !== "") { // if it has a hashtag in the href link.
+        var hash = this.hash; // the variable hash is set to the link.
+
+        $('html, body').animate({scrollTop: $(hash).offset().top}, 800, function(){
+            window.location.hash = hash; // the page scrolls to the corresponding id tag smoother than creamy peanut butter.
+        });
+    }
+});
